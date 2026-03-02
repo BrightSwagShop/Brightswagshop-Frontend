4607ed2798410dce16011bec40a8efe316bc8a2b
LeFrommage <bryan.fouda@student.hogent.be>
2026-03-02 10:36:26 +0100

    winkelwagenPage aangemaakt

--- frontend/src/routing/router.tsx
+++ frontend/src/routing/router.tsx
@@ -14,6 +14,7 @@ import Users from "../Pages/Admin/Users";
 import Products from "../Pages/Admin/Products";
 import Bugs from "../Pages/Admin/Bugs";
 import Settings from "../Pages/Admin/Settings";
+import Winkelwagen from "../Pages/WinkelwagenPage";
 
 export const router = createBrowserRouter([
   // Zonder header/footer
@@ -27,7 +28,7 @@ export const router = createBrowserRouter([
       { index: true, element: <App /> },
       { path: "category/:category", element: <CategoryItemsPage /> },
       { path: "detailpage", element: <DetailPageItem /> },
-
+      { path: "winkelwagen", element: <Winkelwagen />},
       {
         path: "admin",
         element: <AdminLayout />,
