17a7e222fe49c4ca57a15ae26be14adb1694fb56
bryan <147742287+Lefrommage@users.noreply.github.com>
2026-03-02 10:48:38 +0100

    Revert "Feature/winkelwagen"

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,75 +1,7 @@
-import logo from "../assets/Brightest-logo's/logo.png";
-import { FaMicrosoft } from "react-icons/fa";
-
 const Login = () => {
   return (
-    <div className="min-h-screen bg-gray-200 flex flex-col">
+    <div>Login</div>
+  )
+}
 
-      {/* Content */}
-      <div className="flex flex-1 flex-col items-center justify-center">
-
-        {/* Logo */}
-        <div>
-          </div>
-            <img src={logo} alt="Brightest logo" className="h-60 w-auto mb-4 -mt-10" />
-          <div>
-        </div>
-
-        {/* Card */}
-        <div className="w-full max-w-5xl rounded-lg overflow-hidden shadow-lg">
-
-          {/* Form section */}
-          <div className="bg-white px-16 py-12 flex flex-col items-center">
-            <form className="w-full max-w-sm space-y-6">
-
-              <input
-                type="text"
-                placeholder="Gebruikersnaam"
-                className="w-full border-b border-gray-400 focus:outline-none focus:border-yellow-400 py-2 bg-transparent font-semibold"
-              />
-
-              <input
-                type="password"
-                placeholder="Wachtwoord"
-                className="w-full border-b border-gray-400 focus:outline-none focus:border-yellow-400 py-2 bg-transparent font-semibold"
-              />
-
-            <button
-              type="button"
-              className="w-full bg-[#F4C709] font-semibold hover:scale-102 transition-all duration-300 ease-in-out rounded-md py-2 font-medium text-white font-ttnorms cursor-pointer"
-            >                
-              Login
-              </button>
-            </form>
-
-            <div className="my-6 text-black font-bold">of</div>
-
-            <button
-              type="button"
-              className="flex items-center gap-2 border px-4 py-2 rounded-md bg-white cursor-pointer shadow-sm border-1 font-ttnorms hover:scale-105 transition-transform duration-200 ease-in-out"
-            >
-              <FaMicrosoft />
-              Sign in with Microsoft
-            </button>
-          </div>
-
-          {/* Bottom section */}
-          <div className="bg-gray-800 text-white px-16 py-8 flex justify-between text-sm">
-            <div>
-              <p className="font-semibold mb-2">Publieke gebruikersnaam:</p>
-              <p>standaard_gebruiker</p>
-            </div>
-
-            <div>
-              <p className="font-semibold mb-2">Publiek wachtwoord:</p>
-              <p>Pass123!</p>
-            </div>
-          </div>
-
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Login;
+export default Login
